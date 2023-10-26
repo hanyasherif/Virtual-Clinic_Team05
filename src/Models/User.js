00e833d6c91c74f9406d6e824f3e4a91b7a5e5d5
hanyasherif <hanyasherif974@gmail.com>
2023-10-26 23:46:04 +0200

    Update User.js

--- src/Models/User.js
+++ src/Models/User.js
@@ -67,5 +67,5 @@ const userSchema = new Schema({
       }
   }, { timestamps: true });
   
-  const User = mongoose.model('User', patientSchema);
+  const User = mongoose.model('User', userSchema);
   module.exports = User;
