b1b2a98243c7c6cf87745e7e66370e15f0a5ea04
hanyasherif <hanyasherif974@gmail.com>
2023-12-07 21:28:11 +0100

    .

--- src/App.js
+++ src/App.js
@@ -4,22 +4,36 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 require("dotenv").config();
 // const {createMedicine, getMedicine, updateMedicine} = require("./Routes/MedicineController");
-const {addAdministrator, removeUser, checkUsername, getUsers, searchByName, searchBySpec, searchByNameSpec, viewDoctors, getDoctorInfo, getSpecs, filterSpecs, filterByDate, filterDateSpecs} = require("./Routes/userController");
+const {addAdminstrator, getUsers , registerPatient, deleteUser} = require("./Routes/userController");
+const {createPres , viewPatientPrescriptions , filterPrescriptions} = require("./Routes/PrescriptionController");
+const {adminAddPackage , adminDeletePackage , adminUpdatePackage , getPacakges} = require("./Routes/AdminController");
 
 const MongoURI = process.env.MONGO_URI ;
+
+//THIS IS THE TASK CODE TO GUIDE YOUUU
+
+
+
+
+
 
 //App variables
 const app = express();
 const port = process.env.PORT || "8000";
+const cors = require('cors');
 app.get('/', (req, res) =>{
   res.json({mssg: 'Welcome to the app'})
 })
+// const medicine = require('./Models/Medicine');
+// #Importing the userController
 
 // configurations
 // Mongo DB
+app.use(cors());
 mongoose.connect(MongoURI)
 .then(()=>{
   console.log("MongoDB is now connected!")
+
 // Starting server
  app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
@@ -36,17 +50,24 @@ app.get("/home", (req, res) => {
 // #Routing to userController here
 
 app.use(express.json())
-app.post("/addAdministrator", addAdministrator);
-app.delete("/removeUser", removeUser);
-app.post("/checkUsername", checkUsername);
-app.get("/getAllUsers", getUsers);
-app.get("/searchByName",searchByName);
-app.get("/searchBySpec",searchBySpec);
-app.get("/searchByNameSpec",searchByNameSpec);
-app.get("/viewDoctors", viewDoctors);
-app.get("/getDoctorInfo", getDoctorInfo);
-app.get("/getSpecs", getSpecs);
-app.get("/filterSpecs/:spec", filterSpecs);
-app.get("/filterDate/:date", filterByDate);
-app.get("/filterDateSpecs", filterDateSpecs)
+app.post("/addAdminstrator", addAdminstrator);
+app.post("/admin/addPackage", adminAddPackage);
+app.delete("/admin/deletePackage", adminDeletePackage);
+app.put("/admin/updatePackage", adminUpdatePackage);
+app.delete("/deleteUser/:username", deleteUser);
+app.post("/createPatient",registerPatient);
+app.get("/users", getUsers);
 
+app.get("/packs", getPacakges);
+app.delete("/deleteUser/:username", deleteUser);
+//app.put("/addFamMem/:username", addFamMem);
+
+// app.put("/updateMedicine/:id", updateMedicine);
+
+//// Prescription routes
+app.post("/addPrescription",createPres);
+app.get("/viewPrescription/:username", viewPatientPrescriptions);
+app.get("/filterPrescription", filterPrescriptions);
+/*
+                                                    End of your code
+*/
