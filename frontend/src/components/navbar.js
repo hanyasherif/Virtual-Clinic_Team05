b063c05b88a08ea09a218b84c8fba9933663fb59
mellegyy <127795744+mellegyy@users.noreply.github.com>
2023-12-11 17:24:35 +0300

    IGN

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,16 +1,16 @@
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
+    return(
+        <header>
+            <div className="container">
+                <Link to="/">
+                    <h1>El7a2ny</h1>
+                </Link>
+            </div>
+        </header>
 
-  return (
-    <header>
-      <div className="container">
-        <Link to="/">
-          <h1>Clinic Register</h1>
-        </Link>
-      </div>
-    </header>
-  )
+    )
 }
 
 export default Navbar
