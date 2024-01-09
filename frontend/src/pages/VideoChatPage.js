63285515a2ff116ef0c2efa984b50cfcc148bf83
hanyasherif <hanyasherif974@gmail.com>
2024-01-09 22:42:30 +0100

    chatEdit

--- frontend/src/pages/VideoChatPage.js
+++ frontend/src/pages/VideoChatPage.js
@@ -161,64 +161,64 @@ useEffect(() => {
 }, []);
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-       <Box sx={{ display: 'flex' }}>
-        <CssBaseline />
-        <AppBar position="absolute" open={open}>
-          <Toolbar
-            sx={{
-              pr: '24px', // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: '36px',
-                ...(open && { display: 'none' }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              El7a2ny Clinic Patient Page
-            </Typography>
-            <Button color="inherit" onClick={handleLogout}>Logout</Button>
-            <IconButton color="inherit">
-              <Badge badgeContent={0} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'flex-end',
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List component="nav">
-            {mainListItems}
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
-          </List>
-        </Drawer>
+    // <ThemeProvider theme={defaultTheme}>
+    //    <Box sx={{ display: 'flex' }}>
+    //     <CssBaseline />
+    //     <AppBar position="absolute" open={open}>
+    //       <Toolbar
+    //         sx={{
+    //           pr: '24px', // keep right padding when drawer closed
+    //         }}
+    //       >
+    //         <IconButton
+    //           edge="start"
+    //           color="inherit"
+    //           aria-label="open drawer"
+    //           onClick={toggleDrawer}
+    //           sx={{
+    //             marginRight: '36px',
+    //             ...(open && { display: 'none' }),
+    //           }}
+    //         >
+    //           <MenuIcon />
+    //         </IconButton>
+    //         <Typography
+    //           component="h1"
+    //           variant="h6"
+    //           color="inherit"
+    //           noWrap
+    //           sx={{ flexGrow: 1 }}
+    //         >
+    //           El7a2ny Clinic Patient Page
+    //         </Typography>
+    //         <Button color="inherit" onClick={handleLogout}>Logout</Button>
+    //         <IconButton color="inherit">
+    //           <Badge badgeContent={0} color="secondary">
+    //             <NotificationsIcon />
+    //           </Badge>
+    //         </IconButton>
+    //       </Toolbar>
+    //     </AppBar>
+    //     <Drawer variant="permanent" open={open}>
+    //       <Toolbar
+    //         sx={{
+    //           display: 'flex',
+    //           alignItems: 'center',
+    //           justifyContent: 'flex-end',
+    //           px: [1],
+    //         }}
+    //       >
+    //         <IconButton onClick={toggleDrawer}>
+    //           <ChevronLeftIcon />
+    //         </IconButton>
+    //       </Toolbar>
+    //       <Divider />
+    //       <List component="nav">
+    //         {mainListItems}
+    //         <Divider sx={{ my: 1 }} />
+    //         {secondaryListItems}
+    //       </List>
+    //     </Drawer>
         <div className="Chat">
       {!showChat ? (
         <div className="joinChatContainer">
@@ -231,10 +231,10 @@ useEffect(() => {
       )}
     </div>
 
-    </Box>
+    // </Box>
      
 
-    </ThemeProvider>
+    // </ThemeProvider>
   );
 
 
