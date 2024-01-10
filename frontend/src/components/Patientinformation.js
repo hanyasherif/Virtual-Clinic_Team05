e5330b1891e55affe96dc5817455c739c516d654
AseelAmrr <127628826+AseelAmrr@users.noreply.github.com>
2024-01-10 04:37:13 +0100

    backend patientprofile only

--- frontend/src/components/Patientinformation.js
+++ frontend/src/components/Patientinformation.js
@@ -1,89 +1,266 @@
-import React, { useState } from 'react';
+import * as React from 'react';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import MuiDrawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Link from '@mui/material/Link';
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { mainListItems, secondaryListItems } from './listItemsDoc';
+import Button from "@mui/material/Button";
+import UsersList from './UsersList';
+import Meeting from './Appointments';
+import ViewHealthRecords from './ViewHealthRecords';
+import WalletDoc from './WalletDoc';
+import AddAppointment from './AddAppointments';
 import { useLocation } from 'react-router-dom';
-import axios from 'axios';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="http://localhost:3000">
+        El7a2ny
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
 
-function UserProfile() {
-    const [showMore, setShowMore] = useState(false);
-    const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const [Patient, setPatientData] = useState({});
-  const getPatientData= async() =>{
-    const PatientId = searchParams.get('Patient');
-    const Id = searchParams.get('Id');
-    try{const response = await axios.get(`http://localhost:8000/GetFullData?Id=${Id}&&Id1=${PatientId}
-    `); 
+    
+  );
+}
 
-    const P = response.data[0];
+const drawerWidth = 240;
 
-    setPatientData(P);
-    console.log(Patient); 
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+//   backgroundColor: '#004E64', // New background color
+background: 'linear-gradient(to right, #004E64, #0088A8)',
 
-  }
-    catch (error) {
-      alert('An error occurred:', error);
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
+const buttonStyle = {
+  backgroundColor: '#0074d9',
+  color: '#fff',
+  border: 'none',
+  padding: '10px 20px',
+  cursor: 'pointer',
+  display: 'block', // Make the button a block-level element
+  margin: '0 auto', // Center the button horizontally
+};
+
+const containerStyle = {
+  display: 'grid',
+  gridTemplateColumns: '1fr',
+  gap: '20px',
+  alignItems: 'center',
+  justifyItems: 'center',
+  maxWidth: '400px',
+  margin: '0 auto',
+  background: '#fff',
+  padding: '20px',
+  boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+  
+};
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function UserProfile() {
+
+  const [open, setOpen] = React.useState(true);
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  const handleLogout = async (e) => {
+    try {
+      await fetch(`http://localhost:8000/logout`,{credentials: 'include'});
+      window.location.href = 'http://localhost:3000/';
+    } catch (error) {
+      console.error('Error:', error);
     }
-  }
-  const chat= async() =>{
-        localStorage.setItem('partner', searchParams.get('Patient'));
-        window.location.href='http://localhost:3000/ChatPage'
-  }
+  };
+  const location = useLocation();
+const searchParams = new URLSearchParams(location.search);
+const [Patient, setPatient] = useState(); 
+const PatientId = searchParams.get('Patient');
+  const Id = searchParams.get('Id');
+ 
 
-  const Videochat= async() =>{
-    localStorage.setItem('partner', searchParams.get('Patient'));
-    window.location.href='http://localhost:3000/VideoChatPage'
+
+      useEffect(() => {
+        const getPatient = async () => {
+          try {
+            const response = await axios.get(`http://localhost:8000/getUserById/${PatientId}`);
+            setPatient(response.data);
+          } catch (error) {
+            console.error('Error fetching patients:', error.message);
+            
+          }
+        };
+        console.log("My Patientt" , Patient);
+        getPatient();
+      }, []); 
+
+const chat= async() =>{
+      localStorage.setItem('partner', searchParams.get('Patient'));
+      window.location.href='http://localhost:3000/ChatPage'
+}
+
+const Videochat= async() =>{
+  localStorage.setItem('partner', searchParams.get('Patient'));
+  window.location.href='http://localhost:3000/VideoChatPage'
 }
 
 
 
 
-  const toggleShowMore = () => {
-    setShowMore(!showMore);
-  };
-  const buttonStyle = {
-    backgroundColor: '#0074d9',
-    color: '#fff',
-    border: 'none',
-    padding: '10px 20px',
-    cursor: 'pointer',
-    display: 'block', // Make the button a block-level element
-    margin: '0 auto', // Center the button horizontally
-  };
 
-  const containerStyle = {
-    display: 'grid',
-    gridTemplateColumns: '1fr',
-    gap: '20px',
-    alignItems: 'center',
-    justifyItems: 'center',
-    maxWidth: '400px',
-    margin: '0 auto',
-    background: '#fff',
-    padding: '20px',
-    boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-    
-};
-    return (
-      <div className="profile-container">
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: '24px', // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              El7a2ny Clinic Doctor Page
+            </Typography>
+            <Button color="inherit" onClick={handleLogout}>Logout</Button>
+            <IconButton color="inherit">
+              <Badge badgeContent={0} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+            {mainListItems}
+            <Divider sx={{ my: 1 }} />
+            {secondaryListItems}
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+  <Grid container spacing={3}>
+    {/* Grid for WalletDoc and ViewHealthRecords */}
+    {Patient && (
+    <div className="profile-container">
         <div className="profile-info">
           <h2>Hello Dr</h2>
           <p>name: {Patient.username}</p>
           <p>Email: {Patient.email}</p>
           <div style={containerStyle}>
-          <button  onClick={getPatientData}>
-           Patient Data
-          </button>
+        
           <button  onClick={ chat}>
            Chat
           </button>
-          <button style={buttonStyle} onClick={toggleShowMore}>
-            {showMore ? 'Show Less' : 'Show More'}
-          </button>
+          
           <button  onClick={ Videochat}>
           VideoChat
           </button>
         </div>
-        {showMore && (
+        
           <div>
           <p>dateOfBirth: {Patient.dateOfBirth}</p>
           <p>gender: {Patient.gender}</p>
@@ -93,11 +270,23 @@ function UserProfile() {
           <p>emergencyContactMobileNumber: {Patient.emergencyContactMobileNumber}</p>
           <p>emergencyContactFullname: {Patient.emergencyContactFullname}</p>            {/* Add more additional information here */}
           </div>
-        )}
+        
       </div>
         </div>
+)}
+  </Grid>
+  <Copyright sx={{ pt: 4 }} />
+</Container>
 
-    );
-  }
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
 
-export default UserProfile;
+
+
+
+
+
+
